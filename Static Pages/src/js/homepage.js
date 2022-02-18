ef027e31b6a0b53ba92d879028696bb22d1d10db
Cusanity <wyc935398521@gmail.com>
2022-02-17 21:26:05 -0500

    Completed alerts for 1. Bad email format 2. Unmatched passwords #25

--- Static Pages/src/js/homepage.js
+++ Static Pages/src/js/homepage.js
@@ -1,27 +1,38 @@
 function signUp() {
-    if (password_input.value !== confirm_password_input.value) {
-        document.querySelector("#diff_pwd_msg").style.display = "block"
-        document.querySelector("#same_pwd_msg").style.display = "none"
+    if (password_input.val().length === 0 && confirm_password_input.val().length === 0)
+        return
+    if (password_input.val() !== confirm_password_input.val()) {
+        pwd_msg.attr("class", "alert alert-danger alert-dismissable")
+        pwd_msg.html("Passwords do not match")
+        pwd_msg.css("display", "block")
     } else {
-        document.querySelector("#same_pwd_msg").style.display = "block"
-        document.querySelector("#diff_pwd_msg").style.display = "none"
+        pwd_msg.attr("class", "alert alert-success alert-dismissable")
+        pwd_msg.html("Welcome")
+        pwd_msg.css("display", "block")
     }
     password_input.value = ""
     confirm_password_input.value = ""
 }
 
-let btn = document.querySelector("#btn-signup")
-btn.addEventListener("click", signUp);
-let email_phone_input = document.querySelector("#email_phone_input")
-let password_input = document.querySelector("#password_input")
-let confirm_password_input = document.querySelector("#confirm_password_input")
-email_phone_input.addEventListener("input", (event) => {
-    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value))
-    {
-        console.log("working email")
+
+const checkEmailFormat = (event) => {
+    const email_input = event.target.value
+    if (email_input.length === 0 || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.(com|cn|edu))+$/.test(email_input)) {
+        console.log("asas")
+        email_msg.css("display", "none")
+    } else {
+        console.log("bbbb")
+        email_msg.css("display", "block")
     }
-    else {
-        console.log("invalid email")
-    }
-    return false
-})
+}
+
+const signup_btn = $("#btn-signup")
+const email_phone_input = $("#email_phone_input")
+const password_input = $("#password_input")
+const confirm_password_input = $("#confirm_password_input")
+const pwd_msg = $("#pwd_msg")
+const email_msg = $("#email_msg")
+
+
+signup_btn.on("click", signUp)
+email_phone_input.on("input", checkEmailFormat)
