1e03d768b717373c7b7ebdc8ee947a3d41a31381
Andy <awu46@buffalo.edu>
2022-03-04 13:36:02 -0500

    #45 Completed prototype for storing created tasks

--- Static_Pages/php/create-task.php
+++ Static_Pages/php/create-task.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $res = "Creation successful!";
 $servername = "128.205.36.4";
 $username = "awu46";
@@ -10,18 +14,20 @@ if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 
-echo "post found!";
 $name = $_POST["name"];
 $desc = $_POST["desc"];
 $due_date = $_POST["dueDate"];
 $notes = $_POST["notes"];
 
+$card_name = $_POST["cardName"]; // Force unique card names for each individual
+$card_id = 12; // Card name should be used to find corresponding unique ID global to all users
+
 // $email = $_SESSION['email']
 $email = "t@t.com";
 
-$stmt = $mysqli->prepare("INSERT INTO cards(name, description, due_date, extra_notes, email) VALUES (?, ?, ?, ?, ?)");
+$stmt = $mysqli->prepare("INSERT INTO tasks(name, description, due_date, extra_notes, card_id, email) VALUES (?, ?, ?, ?, ?, ?)");
 // echo $mysqli->error;
-$stmt->bind_param("sssss", $name, $desc, $due_date, $notes, $email);
+$stmt->bind_param("ssssss", $name, $desc, $due_date, $notes, $card_id, $email);
 $stmt->execute();
 
 $mysqli->close();
