5d272bde7111efcbd872b19616a73fcf1d74ab3f
Feliricki <feliricki01@gmail.com>
2022-03-06 20:26:11 -0500

    testing php #39

--- Static_Pages/php/card-retrieve-static.php
+++ Static_Pages/php/card-retrieve-static.php
@@ -10,7 +10,7 @@ $mysqli = new mysqli($servername, $username, $password, $database);
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
-    echo "<p>connection failed</p>"
+    echo "<p>connection failed</p>";
 }
 session_start();
 
@@ -21,9 +21,8 @@ $results = $mysqli->query($sql);
 $card_tasks = array(); # associative array -> (card_name => [tasks 1, task 2, ....])
 
 if ($results->num_rows > 0){
-    $index = 0
     while($row = $results->fetch_assoc()){
-        $card_tasks[$row["name"]] = array();
+        $card_tasks += array($row["name"] => array());
         $_SESSION["card_name"] = $row["name"];    // Used for hardcoded stuff
         $_SESSION["card_description"] = $row["description"];
         break;
@@ -33,39 +32,39 @@ else{
 }   
 }
 
-// Retrieve info
-// Does not handle the case where there is no card
-// This only works if the card name is passed as the parameter in "name" instead of the label
-$sql = "SELECT * FROM tasks";
-$results = $mysql->query($sql);
-$_SESSION["tasks"] = array()
+// // Retrieve info
+// // Does not handle the case where there is no card
+// // This only works if the card name is passed as the parameter in "name" instead of the label
+// $sql = "SELECT * FROM tasks";
+// $results = $mysql->query($sql);
+// $_SESSION["tasks"] = array()
 
-if ($results->num_rows > 0){
-    while($row = $results->fetch_assoc()){
-        $card_title = $row["card_id"]
-        if (array_key_exists($card_title, $card_tasks)){    # if there is card associated with the task
-            $card_tasks[$card_title][] = $row["description"]; 
-            break;
-        }
-        if(strtolower($card_title) == strtolower($_SESSION["card_name"]) ){ // hardcoded stuff
-            $_SESSION["tasks"][] = $row["description"];
-        }
-    }
-else{
-    echo "No tasks in database. <br>"
-}
-}
+// if ($results->num_rows > 0){
+//     while($row = $results->fetch_assoc()){
+//         $card_title = $row["card_id"]
+//         if (array_key_exists($card_title, $card_tasks)){    # if there is card associated with the task
+//             $card_tasks[$card_title][] = $row["description"]; 
+//             break;
+//         }
+//         if(strtolower($card_title) == strtolower($_SESSION["card_name"]) ){ // hardcoded stuff
+//             $_SESSION["tasks"][] = $row["description"];
+//         }
+//     }
+// else{
+//     echo "No tasks in database. <br>"
+// }
+// }
 
-// Print all tasks associated with a card name
-function print_tasks($card_name){
-    if (in_array($card_name, $GLOBAL['card_tasks'])){
-        $tasks = $GLOBAL['card_tasks'][$card_name]
-        foreach ($tasks as $task){
-            echo "<li><input type='test' value="$task"></li>"
-        }
-    } else{
-        echo "<li></li>"
-    }
-}
+// // Print all tasks associated with a card name
+// function print_tasks($card_name){
+//     if (in_array($card_name, $GLOBAL['card_tasks'])){
+//         $tasks = $GLOBAL['card_tasks'][$card_name]
+//         foreach ($tasks as $task){
+//             echo "<li><input type='test' value="$task"></li>"
+//         }
+//     } else{
+//         echo "<li></li>"
+//     }
+// }
 
 ?>
