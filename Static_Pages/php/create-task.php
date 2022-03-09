bf3caab553f023cf0faae40af113f0764a063a27
Andy <awu46@buffalo.edu>
2022-03-08 22:31:29 -0500

    Progress with #50, currently debugging issue with tasks data not being stored

--- Static_Pages/php/create-task.php
+++ Static_Pages/php/create-task.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $res = "Creation successful!";
 $servername = "128.205.36.4";
 $username = "awu46";
@@ -19,15 +15,41 @@ $name = $_POST["name"];
 $desc = $_POST["desc"];
 $due_date = $_POST["dueDate"];
 $notes = $_POST["notes"];
-
-$card_name = $_POST["cardName"]; // Force unique card names for each individual
-$card_id = 12; // Card name should be used to find corresponding unique ID global to all users
+$card_name = $_POST["cardName"]; // Force unique (case-insensitive) card names for each individual
 
 $email = $_SESSION['email'];
+$card_id = NULL; // Card name should be used to find corresponding unique cardID
+
+if($card_name != "") {
+	$card_stmt = $mysqli->prepare("SELECT * FROM cards WHERE name = ? AND email = ?");
+    $card_stmt->bind_param("ss", $card_name, $email);
+    $card_stmt->execute();
+    $card_stmt->store_result();
+
+    $result = $card_stmt->get_result();
+
+    // Assume card doesn't exist unless you find it in db
+    $card_exists = False;
+    if($result->num_rows > 0) {
+        $card_exists = True;
+        // Find way to do this w/o while loop.
+        // There will only ever be one row since no duplicate card_names allowed
+        while($row = $result->fetch()) {
+            $card_id = $row["cardID"];
+            break;
+        }
+    }
+    else {
+        // Trigger alert describing card_name issue and redirect back to form once user confirms
+        echo '<script type="text/javascript">
+        alert("Cannot assign task to a card that doesn\'t exist!");
+        window.location.href = "../create-task.html"</script>';
+    }
+}
 
 $stmt = $mysqli->prepare("INSERT INTO tasks(name, description, due_date, extra_notes, card_id, email) VALUES (?, ?, ?, ?, ?, ?)");
 // echo $mysqli->error;
-$stmt->bind_param("ssssss", $name, $desc, $due_date, $notes, $card_id, $email);
+$stmt->bind_param("ssssis", $name, $desc, $due_date, $notes, $card_id, $email);
 $stmt->execute();
 
 header("Location: ../RUD.html");
