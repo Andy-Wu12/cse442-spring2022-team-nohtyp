224fcc482eefb7ffd7e4145644bb7aa0a7cafc07
Cusanity <wyc935398521@gmail.com>
2022-03-17 21:49:18 -0400

    #44 Completed notification for tasks

--- Static_Pages/src/js/notification.js
+++ Static_Pages/src/js/notification.js
@@ -1,14 +1,36 @@
 export function showNotification(tasksJsonArray) {
-    const timeDiffObj = timeDifference(new Date(tasksJsonArray[0]["due_date"]), new Date())
-    const newNotification = new Notification(tasksJsonArray[0]["name"], {
+    // const timeDiffObj = timeDifference(new Date(tasksJsonArray[0]["due_date"]), new Date())
+    const summaryNotification = new Notification("", {
         // body: Date.parse(tasksJsonArray[0]["due_date"])
-        body: "Due in " + timeDiffObj["days"] + " days " + timeDiffObj["hours"] + " hours " + timeDiffObj["minutes"]
-            + " minutes " + timeDiffObj["seconds"] + " seconds"
+        body:generateSummary(tasksJsonArray)
     })
+}
 
-    newNotification.onclick = (e) => {
-        alert(e)
-    }
+function stringifyTaskArray(taskArr, param = "name"){
+    let res = ""
+    taskArr.forEach(task => res += ("- " + task[param] + "\r\n"))
+    return res
+}
+
+function generateSummary(tasksJsonArray){
+    const tasksPastDue = tasksJsonArray.filter(task => timeDifference(new Date(task["due_date"]), new Date())["days"] > 0)
+    const tasksDueToday = tasksJsonArray.filter(task => timeDifference(new Date(task["due_date"]), new Date())["days"] === 0)
+    const tasksBeforeDue = tasksJsonArray.filter(task => timeDifference(new Date(task["due_date"]), new Date())["days"] < 0)
+    if(tasksDueToday.length === 0 && tasksDueToday.length === 0 && tasksDueToday.length === 0)
+        return "Good job!\r\nYou've finished all your tasks!"
+
+    const tasksDueTodayStr = stringifyTaskArray(tasksDueToday)
+    const tasksPastDueStr = stringifyTaskArray(tasksPastDue)
+    const tasksBeforeDueStr = stringifyTaskArray(tasksBeforeDue)
+
+    let summary = "You have:\r\n"
+    if(tasksPastDue.length > 0)
+        summary += tasksPastDue.length + " task(s) past due date\r\n" + tasksPastDueStr
+    if(tasksDueToday.length > 0)
+        summary += tasksDueToday.length + " task(s) due today\r\n" + tasksDueTodayStr
+    if(tasksBeforeDue.length > 0)
+        summary += tasksBeforeDue.length + " task(s) coming up\r\n" + tasksBeforeDueStr
+    return summary
 }
 
 export function timeDifference(date1, date2) {
