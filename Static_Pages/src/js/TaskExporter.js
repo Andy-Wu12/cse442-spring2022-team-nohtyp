d6014f3146e39ec7bbd9373d26f5fedf5d413e49
Andy Wu <awpats12@gmail.com>
2022-04-08 11:35:18 -0400

    Completed #48, calendar files can now be generated and exported for local use.

--- Static_Pages/src/js/TaskExporter.js
+++ Static_Pages/src/js/TaskExporter.js
@@ -6,9 +6,10 @@ import {getSessionEmail, redirectByLoginStatus, getJsonTasksArray} from "./sessi
 // (Handles memory leak issue)
 let icsFile = null;
 
+// Returns link to downloadable .ics file
 function TaskExporter() {
     // Export ALL tasks into one .ics file.
-    if(localStorage.userTaskCounter === null) {
+    if(isNaN(localStorage.userTaskCounter)) {
         // Use this number as part of the unique identifier portion of UID field
         localStorage.userTaskCounter = 0;
     }
@@ -22,30 +23,40 @@ function TaskExporter() {
     }
 
     function getNextTaskCounter() {
-        let nextCounterVal = localStorage.userTaskCounter + 1
-        localStorage.userTaskCounter += 1
+        // parseInt necessary b/c local storage only stores strings as values
+        let nextCounterVal = parseInt(localStorage.userTaskCounter) + 1;
+        console.log(nextCounterVal);
+        localStorage.userTaskCounter = nextCounterVal;
 
-        return nextCounterVal
+        return nextCounterVal;
     }
 
     function addEventDataToICSFile(taskJson) {
         let data_blob = "BEGIN:VEVENT\n";
 
         // Use session email as LHS of uid + incremented task counter to guarantee uniqueness
-        let uid = `${getSessionEmail()}${getNextTaskCounter()}@cse442NohTypDotPlan`
-        let due_date = convertDate(taskJson["due_date"])
-        let summary = taskJson["name"]
-        let desc = taskJson["description"]
+        let uid = `${getSessionEmail()}${getNextTaskCounter()}@cse442NohTypDotPlan`;
+        let due_date = null;
+        let start_date = convertDate(Date.now());
+        if(taskJson["due_date"] != null) {
+            console.log("Task with due date!")
+            due_date = convertDate(taskJson["due_date"]);
+        }
+        let summary = taskJson["name"];
+        let desc = taskJson["description"];
 
         data_blob +=
             `UID:${uid}\n` +
-            `DTEND:${due_date}\n` +
+            `DTSTART:${start_date}\n` +
             `SUMMARY:${summary}\n` +
             `DESCRIPTION:${desc}\n`;
 
+        if(due_date != null) {
+            data_blob += `DTEND:${due_date}\n`;
+        }
         data_blob += "END:VEVENT\n";
 
-        console.log(data_blob);
+        // console.log(data_blob);
         return data_blob;
     }
 
@@ -67,15 +78,16 @@ function TaskExporter() {
         "VERSION:2.0\n" +
         "PRODID:-//Dot-Plan-Cal//EN\n" +
         "CALSCALE:GREGORIAN\n" +
-        "METHOD:PUBLISH\n"
+        "METHOD:PUBLISH\n";
 
     // Add all event data after header
     for(let i = 0; i < tasksJsonArray.length; i++) {
-        blob += addEventDataToICSFile(tasksJsonArray);
+        blob += addEventDataToICSFile(tasksJsonArray[i]);
     }
-    blob += "END:VCALENDAR";
+    blob += "END:VCALENDAR\n";
 
-    let data = new File([blob], "DotPlanEvents.ics", {type: "text/plain"});
+    console.log(blob);
+    let data = new File([blob], "DotPlanEvents.ics", {type: "text/calendar"});
 
     // If we are replacing a previously generated file we need to
     // manually revoke the object URL to avoid memory leaks.
@@ -84,13 +96,16 @@ function TaskExporter() {
     }
 
     icsFile = window.URL.createObjectURL(data);
+    console.log(icsFile);
     return icsFile;
 }
 
+
 // Puts TaskExporter() into DOM's global scope so it can be called directly from <a> href tag
-window.TaskExporter = TaskExporter;
+// window.TaskExporter = TaskExporter;
 
 /////////////////////     Alternative way to get download data    ////////////////////////////
-// // Import this file as a script, create an <a> tag in your html with id="export-button"
-// let action_tag = document.getElementById("export-button");
-// action_tag.href = TaskExporter();
+// Import this file as a script, create an <a> tag in your html with id="export-button"
+let action_tag = document.getElementById("export-button");
+action_tag.setAttribute('href', TaskExporter());
+action_tag.setAttribute('download', "DotPlanEvents.ics");
